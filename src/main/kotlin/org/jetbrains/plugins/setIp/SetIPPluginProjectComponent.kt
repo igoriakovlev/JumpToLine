96b423832020a6ce066e913db8c3ee2dd5be69cd
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2019-11-21 22:24:14 +0300

    Added debug helper for instrumentation

--- src/main/kotlin/org/jetbrains/plugins/setIp/SetIPPluginProjectComponent.kt
+++ src/main/kotlin/org/jetbrains/plugins/setIp/SetIPPluginProjectComponent.kt
@@ -11,10 +11,46 @@ import com.intellij.debugger.impl.DebuggerSession
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
 import com.intellij.xdebugger.impl.XDebugSessionImpl
+import org.jetbrains.plugins.setIp.injectionUtils.MethodName
+import org.jetbrains.plugins.setIp.injectionUtils.dumpClass
+import org.jetbrains.plugins.setIp.injectionUtils.getAvailableGotoLines
+import org.jetbrains.plugins.setIp.injectionUtils.updateClassWithGotoLinePrefix
+import java.io.File
+import java.nio.file.Files
 
 class SetIPPluginProjectComponent(private val project: Project) : ProjectComponent {
 
+    private fun instrument() {
+        val file = Files.readAllBytes(File("C:\\AAA\\AbstractFindUsagesWithDisableComponentSearchTest\\Original.class").toPath())
+        val methodName = MethodName("findUsagesAndCheckResults", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Lcom/intellij/psi/PsiElement;Lcom/intellij/find/findUsages/FindUsagesOptions;Lcom/intellij/openapi/project/Project;Z)V", null)
+
+        val result = getAvailableGotoLines(
+                ownerTypeName = "org/jetbrains/kotlin/findUsages/AbstractFindUsagesTestKt",
+                targetMethod = methodName,
+                lineTranslator = null,
+                klass = file
+        )
+
+        val line = result?.firstOrNull { it.javaLine == 250 } ?: return
+        val argumentsCount = 7
+
+        val classToRedefine = updateClassWithGotoLinePrefix(
+                targetLineInfo = line,
+                targetMethod = methodName,
+                argumentsCount = argumentsCount,
+                klass = file,
+                commonTypeResolver = CommonTypeResolver(project)
+        )
+
+        if (classToRedefine !== null) {
+            dumpClass(file, classToRedefine)
+        }
+    }
+
     override fun projectOpened() {
+
+        //instrument()
+
         val debuggerListener = object : DebuggerManagerListener {
             override fun sessionAttached(session: DebuggerSession?) {
                 val xSession = session?.xDebugSession as? XDebugSessionImpl ?: return
