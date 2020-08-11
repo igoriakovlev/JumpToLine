d3b0221a42508d28e2cc849edbdc70fb9d6e638a
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-08-11 15:06:45 +0300

    Set license to Apache 2.0

--- src/main/kotlin/org/jetbrains/plugins/jumpToLine/injectionUtils/LocalVariableAnalyzer.kt
+++ src/main/kotlin/org/jetbrains/plugins/jumpToLine/injectionUtils/LocalVariableAnalyzer.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020-2020 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package org.jetbrains.plugins.jumpToLine.injectionUtils
 
 import com.jetbrains.rd.util.getOrCreate
